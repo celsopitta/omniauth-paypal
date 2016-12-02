a29063dfbd2d77430b3161dff2a26e21739500db
Celso Pitta <celso.pitta@gmail.com>
2016-12-02 12:43:23 -0800

    bug_connect

--- lib/omniauth/strategies/paypal.rb
+++ lib/omniauth/strategies/paypal.rb
@@ -71,6 +71,10 @@ module OmniAuth
                })
       end
 
+      def callback_url
+        options[:redirect_uri] || (full_host + script_name + callback_path)
+      end
+
       def raw_info
         @raw_info ||= load_identity()
       end
